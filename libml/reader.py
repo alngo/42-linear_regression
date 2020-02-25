19ac26ebb9f47f41014a1aa4027b96031109a837
alngo <alngo@student.42.fr>
2020-02-25 14:29:26 +0100

    feat(master): fix theta.

--- libml/reader.py
+++ libml/reader.py
@@ -6,7 +6,7 @@
 #    By: alngo <alngo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/24 09:44:24 by alngo             #+#    #+#              #
-#    Updated: 2020/02/25 13:03:10 by alngo            ###   ########.fr        #
+#    Updated: 2020/02/25 14:28:26 by alngo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -28,10 +28,8 @@ class Reader:
         m = scale(self.args[0], self.data['Xmin'][0], self.data['Xmax'][0])
         p0 = np.float64(self.data['theta0'][0])
         p1 = np.float64(self.data['theta1'][0])
-        if m == 0.0 and p0 == 0.0 and p1 == 0.0:
-            return "Error"
         prediction = p0 + (m * p1)
-        if prediction:
+        if prediction != 0:
             return int(upscale(prediction, self.data['ymin'][0],
                 self.data['ymax'][0]))
         else:
