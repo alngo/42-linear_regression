695d838d31fd2a41c98a8153e50c53a97159f282
alngo <alngo@student.42.fr>
2020-03-02 11:26:43 +0100

    feat(model): fix model and test.

--- libml/utils/maths.py
+++ libml/utils/maths.py
@@ -6,7 +6,7 @@
 #    By: alngo <alngo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/24 09:45:07 by alngo             #+#    #+#              #
-#    Updated: 2020/03/02 10:20:46 by alngo            ###   ########.fr        #
+#    Updated: 2020/03/02 11:25:39 by alngo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,6 +24,7 @@ def normalize(list):
     newList = (list - minValue) / (maxValue - minValue)
     return [newList, minValue, maxValue]
 
+
 def denormalize_theta(dataset, normalized_dataset, theta0, theta1, min, max):
     x0 = dataset.iloc[0, 0]
     x1 = dataset.iloc[1, 0]
@@ -33,23 +34,22 @@ def denormalize_theta(dataset, normalized_dataset, theta0, theta1, min, max):
     hy0 = theta0 + (nx0 * theta1)
     hy1 = theta0 + (nx1 * theta1)
 
-    p0 = (x1 / (x1 - x0)) * (upscale(hy0, min, max) - (x0 / x1 * upscale(hy1, min, max)))
-    p1 = (y0 - p0) / x0
-
-
-    print(f"x0: {x0}, x1: {x1}")
-    print(f"x0n: {nx0}, x1n: {nx1}")
-    print(f"y0n: {hy0}, y1n: {hy1}")
-    print(f"y0: {y0}")
-    print(f"{dataset}")
-    print(f"{p0} - {p1}")
+    try:
+        p0 = (x1 / (x1 - x0)) * (upscale(hy0, min, max) - (x0 / x1 * upscale(hy1, min, max)))
+        p1 = (y0 - p0) / x0
+    except ZeroDivisionError:
+        print(f'Zero division error')
+        sys.exit(1)
     return p0, p1
 
 
 def scale(value, min, max):
-    if min == 0 and max == 0:
-        return np.float(0.0)
-    return np.float64((value - min) / (max - min))
+    try:
+        res = np.float64((value - min) / (max - min))
+    except ZeroDivisionError:
+        print(f'Zero division error')
+        sys.exit(1)
+    return res
 
 
 def upscale(value, min, max):
